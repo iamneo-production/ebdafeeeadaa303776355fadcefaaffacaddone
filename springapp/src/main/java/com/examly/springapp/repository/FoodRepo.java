fcb0d805e6f4f487d388e4845ca741f6b0bff7c0
Iamneo <techadmin@iamneo.ai>
2023-08-02 09:09:10 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/repository/FoodRepo.java
+++ springapp/src/main/java/com/examly/springapp/repository/FoodRepo.java
@@ -11,16 +11,16 @@ import com.examly.springapp.model.Food;
 @Repository
 public interface FoodRepo extends JpaRepository <Food , Integer> {
 
-    @Query(value = "SELECT * FROM Food u  WHERE u.foodid = ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM food u  WHERE u.food_id = ?1", nativeQuery = true)
     public Food findbyId(int foodId);
 
-    @Query(value = "SELECT * FROM Food u WHERE u.food_origin = ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM food u WHERE u.food_origin = ?1", nativeQuery = true)
     public Food findbyOrigin (String foodOrigin);
 
-    @Query(value = "SELECT * FROM Food u WHERE u.food_name = ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM food u WHERE u.food_name = ?1", nativeQuery = true)
     public Food findbyName (String foodName);
 
-    @Query(value = "SELECT * FROM Food u WHERE u.food_cusine = ?1  AND u.food_name = ?2" , nativeQuery = true )
+    @Query(value = "SELECT * FROM food u WHERE u.food_cusine = ?1  AND u.food_name = ?2" , nativeQuery = true )
     public List<Food> findCN(String foodCusine , String  foodName);
     
 }
